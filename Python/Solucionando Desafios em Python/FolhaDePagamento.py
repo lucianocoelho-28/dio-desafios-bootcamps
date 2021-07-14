b20134b602654f45f5c9bb9d4101cbbc7aee7a6f
Luciano Coelho <coelho.luciano@icloud.com>
2021-07-14 16:09:55 -0300

    FolhaDePagamento.py

--- Python/Solucionando Desafios em Python/FolhaDePagamento.py
+++ Python/Solucionando Desafios em Python/FolhaDePagamento.py
@@ -1,3 +1,5 @@
+# Folha de Pagamento
+
 #Desafio
 #Precisamos saber quanto uma determinada empresa deve pagar para seus colaboradores, porém temos apenas a quantidade de horas trabalhadas e o valor hora. Escreva um programa que leia o número de um colaborador, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse colaborador. Em seguida, apresente o número e o salário do colaborador, com duas casas decimais.
 
