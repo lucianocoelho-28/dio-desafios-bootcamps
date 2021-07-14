06b8ee46976bd6e2cff1c0809947e68ff5219aa5
Luciano Coelho <coelho.luciano@icloud.com>
2021-07-14 16:10:43 -0300

    PreenchimentoDeVetorI.py

--- Python/Soluções Aritméticas em Python/PreenchimentoDeVetorI.py
+++ Python/Soluções Aritméticas em Python/PreenchimentoDeVetorI.py
@@ -1,3 +1,5 @@
+# Preenchimento de Vetor I
+
 #Desafio
 #Você recebeu o desafio de ler um valor e criar um programa que coloque o valor lido na primeira posição de um vetor N[10]. Em cada posição subsequente, coloque o dobro do valor da posição anterior. Por exemplo, se o valor lido for 1, os valores do vetor devem ser 1,2,4,8 e assim sucessivamente. Mostre o vetor em seguida.
 
